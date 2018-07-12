68909b14ff790f24c9a37f66125048ae3f551b5f
Sven Kreienbrock <sven.kreienbrock@ruhr-uni-bochum.de>
2018-07-12 15:26:33 +0200

    added some tk code

--- python/tk/hw.py
+++ python/tk/hw.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+# experimental first window
 from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-fr1=ttk.namedFrame(root)
+fr1=ttk.Frame(root)
 fr1label=Label(root,text="Hallo Welt",width=20).pack(side='left')
 fr1entry=Entry(root,width=20).pack(side='right')
 
